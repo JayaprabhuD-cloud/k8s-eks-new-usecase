75f4c10f4a4616df29ff981e3e6fb491853098a9
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-07-06 18:06:44 +0530

    ..

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -46,11 +46,11 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
 }
 
 
-# Datasource: AWS Partition
+# Datasource: AWS Partition (e.g., aws, aws-cn, aws-us-gov).
 # Use this data source to lookup information about the current AWS partition in which Terraform is working
+
 data "aws_partition" "current" {}
 
-############################# AWS IAM Open ID Connect Provider ############################################################
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
   thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
